7614382230e836e7d059bae51d17f50bed8bdcb9
Hämäläinen, Mika K <mika.hamalainen@helsinki.fi>
2019-07-24 11:25:48 +0300

    ocr models

--- natas/__init__.py
+++ natas/__init__.py
@@ -4,5 +4,8 @@ from .normalize import is_in_dictionary as _is_in_dictionary
 def normalize_words(words, n_best=10, dictionary=None, all_candidates=True):
 	return _normalize(words, "normalization.pt", n_best=n_best, dictionary=dictionary, all_candidates=all_candidates)
 
+def ocr_correct_words(words, n_best=10, dictionary=None, all_candidates=True):
+	return _normalize(words, "ocr.pt", n_best=n_best, dictionary=dictionary, all_candidates=all_candidates)
+
 def is_correctly_spelled(word, dictionary=wiktionary, cache=True):
 	return _is_in_dictionary(word.lower(), dictionary, _get_spacy(), cache)
