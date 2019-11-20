4f32f0c445092fc63c6e88790b1ab33ca5bb6190
Mika Hämäläinen <mikahama@users.noreply.github.com>
2019-11-20 15:36:58 +0200

    Update test.py

--- test.py
+++ test.py
@@ -10,3 +10,8 @@ model = Word2Vec.load("/Users/mikahama/Downloads/models/model_fi_1820-1917.w2v")
 seed_words = set(open_read("words-in-dictionary").read().split("\n"))
 res = ocr_builder.extract_parallel(seed_words, model, dictionary=seed_words, lemmatize=False, use_freq=False)
 json_dump(res, "fin_parallel.json")
+
+import natas
+
+l = ['land', '.', "f'c", '.', 'courteous', '[', 'ourrtoil', ',', 'f.', ']', 'affable', ',', 'civil', ',', 'gentle', '.', "courte'ously", ',', 'civilly', '.', 'affibly', '.', "courte'ousness", ',', 'civility', ',', 'affabilliy', '.', "cour'tesan", '[', 'cortexana', ',', 'ital', '.', ']', '.a', "cour'tezen", '~', 'lady', 'or', 'gentlewoman', 'belonging', 'to', 'the', 'court', ';', 'a', 'professed', 'strum-', 'pet']
+natas.ocr_correct_words(l)
